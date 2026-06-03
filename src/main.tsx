bafdec2ec5a38d0e032e762863203ab77bb3242c
Yuvan Adarsh Jagannathan <yuvanadarshj@gmail.com>
2026-06-03 10:31:40 -0400

    feat: navbar

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,12 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
+import Navbar from "./components/navbar.tsx";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
+    <Navbar />
     <App />
   </StrictMode>,
-)
+);
