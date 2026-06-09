3d22864efc70f239f7e96035a17b4a07f09b9d48
Yuvan Adarsh Jagannathan <yuvanadarshj@gmail.com>
2026-06-08 22:37:58 -0400

    feat: ReactRouter added for routing

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,12 +1,19 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import App from "./App.tsx";
 import Navbar from "./components/Navbar.tsx";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Dashboard from "./pages/Dashboard.tsx";
+import ProjectDetails from "./pages/ProjectDetails.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <Navbar />
-    <App />
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Dashboard />} />
+        <Route path="/projects/:id" element={<ProjectDetails />} />
+      </Routes>
+    </BrowserRouter>
   </StrictMode>,
 );
